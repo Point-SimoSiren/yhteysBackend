4c96766eaf1fe8b057ae6222ee8d393bb704b0c2
Point-SimoSiren <45048404+Point-SimoSiren@users.noreply.github.com>
2019-07-07 00:16:25 +0300

    salasanat piiloon

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb://sirensimo7:Jeesus7777!@simozon-shard-00-00-e2242.mongodb.net:27017,simozon-shard-00-01-e2242.mongodb.net:27017,simozon-shard-00-02-e2242.mongodb.net:27017/test?ssl=true&replicaSet=Simozon-shard-0&authSource=admin&retryWrites=true',
+    mongoURI: 'mongodb://uname:password@simozon-shard-00-00-e2242.mongodb.net:27017,simozon-shard-00-01-e2242.mongodb.net:27017,simozon-shard-00-02-e2242.mongodb.net:27017/test?ssl=true&replicaSet=Simozon-shard-0&authSource=admin&retryWrites=true',
     secretOrKey: 'SECRET'
 };
